31452000643a836e6b056051c1453cced1f57b13
Kittithee Thaweesuk <Kittithee.T@thairelife.local>
2021-03-09 01:08:06 +0700

    Implement onClickListener in disabled activity

--- app/src/main/java/com/project/help/disabled/DisabledMainActivity.kt
+++ app/src/main/java/com/project/help/disabled/DisabledMainActivity.kt
@@ -15,7 +15,7 @@ import com.project.help.R
 import com.project.help.disabled.model.PostDetails
 
 
-class DisabledMainActivity : AppCompatActivity() {
+class DisabledMainActivity : AppCompatActivity(), View.OnClickListener {
 
     //region Global variable
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
@@ -34,6 +34,93 @@ class DisabledMainActivity : AppCompatActivity() {
     private lateinit var recyclerFeed: RecyclerView
     //endregion Global variable
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disabled_main)
+        supportActionBar?.hide()
+
+        imgProfile = findViewById(R.id.imgProfile)
+        txtFullName = findViewById(R.id.txtFullName)
+        txtUserType = findViewById(R.id.txtUserType)
+        otherMenu = findViewById(R.id.otherMenu)
+        oldPost = findViewById(R.id.oldPost)
+        archiveOfPosts = findViewById(R.id.archiveOfPosts)
+        ratingReqForHelp = findViewById(R.id.ratingReqForHelp)
+        ratingVolunteerForHelp = findViewById(R.id.ratingVolunteerForHelp)
+        spinnerCategory = findViewById(R.id.spinnerCategory)
+        btnPost = findViewById(R.id.btnPost)
+        recyclerFeed = findViewById(R.id.recycler_feed)
+
+        archiveOfPosts.setOnClickListener(this)
+        oldPost.setOnClickListener(this)
+        otherMenu.setOnClickListener(this)
+        btnPost.setOnClickListener(this)
+
+        //region On init
+        setMenuBottomSheet()
+        setToolbar()
+        setRatingUserHeader()
+        setSpinnerCategory()
+
+        val postDetail = intent.getSerializableExtra("PostDetail") as? PostDetails
+        getPosts(postDetail)
+        //endregion On init
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.archiveOfPosts ->  {
+                val intent = Intent(this, ArchiveOfPostsActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.oldPost -> {
+                val intent = Intent(this, OldPostActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.otherMenu -> {
+                val intent = Intent(this, OtherMenu::class.java)
+                startActivity(intent)
+            }
+            R.id.btnPost -> {
+                val intent = Intent(this, PostActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun setMenuBottomSheet() {
+        bottomSheetDisabled = findViewById(R.id.bottom_disabled_sheet)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetDisabled)
+
+        bottomSheetBehavior.setBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomSheet: View, state: Int) {
+                print(state)
+                when (state) {
+
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
+                }
+            }
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+    }
+
+    private fun setToolbar() {
+        iconLeft = findViewById(R.id.iconLeft)
+        iconLeft.visibility = View.INVISIBLE
+    }
+
     private fun setRatingUserHeader() {
 //        ratingReqForHelp.rating = 1.0F
 //        ratingVolunteerForHelp.rating = 5.0F
@@ -41,9 +128,8 @@ class DisabledMainActivity : AppCompatActivity() {
 
     private fun setSpinnerCategory() {
         var categories = arrayOf("หมวดหมู่ผู้พิการ", "การมองเห็น", "การได้ยิน", "การเคลื่อนไหวร่างกาย", "สติปัญญา",
-                "ออทิสติก", "ผู้สูงอายุ")
+            "ออทิสติก", "ผู้สูงอายุ")
 
-//        spinnerCategory.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item)
         var adapter = ArrayAdapter(this, R.layout.color_spinner_layout, categories)
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout)
         spinnerCategory.adapter = adapter
@@ -81,101 +167,18 @@ class DisabledMainActivity : AppCompatActivity() {
 
             // Set content feed
             val item = PostItem(drawable, "User " + (i+1).toString(),
-                    "ช่วยอ่านใบนัดหมอให้ทีครับ", (i+1), 3.0F, 0)
+                "ช่วยอ่านใบนัดหมอให้ทีครับ", (i+1), 3.0F, 0)
             list += item
         }
         list += PostItem(R.drawable.privacypolicy, "",
-                "", 999999, 3.0F, 0)
+            "", 999999, 3.0F, 0)
         return list
     }
 
     private fun addPostList(postList: ArrayList<PostItem>, postDetails: PostDetails): ArrayList<PostItem> {
         var index = postList.size - 1
         postList.add(index, PostItem(R.drawable.privacypolicy, "User " + (index+1).toString(),
-                postDetails.txtPost.toString(), index+1, 5.0F, 0))
+            postDetails.txtPost.toString(), index+1, 5.0F, 0))
         return postList
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_disabled_main)
-        supportActionBar?.hide()
-
-        //region Set menu bottom sheet
-        bottomSheetDisabled = findViewById(R.id.bottom_disabled_sheet)
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetDisabled)
-
-        bottomSheetBehavior.setBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback(){
-            override fun onStateChanged(bottomSheet: View, state: Int) {
-                print(state)
-                when (state) {
-
-                    BottomSheetBehavior.STATE_HIDDEN -> {
-                    }
-                    BottomSheetBehavior.STATE_EXPANDED -> {
-                    }
-                    BottomSheetBehavior.STATE_COLLAPSED -> {
-                    }
-                    BottomSheetBehavior.STATE_DRAGGING -> {
-                    }
-                    BottomSheetBehavior.STATE_SETTLING -> {
-                    }
-                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
-                    }
-                }
-            }
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-            }
-        })
-        //endregion
-
-        //region Back button
-        iconLeft = findViewById(R.id.iconLeft)
-        iconLeft.visibility = View.INVISIBLE
-        //endregion Back button
-
-        //region Set variable
-        imgProfile = findViewById(R.id.imgProfile)
-        txtFullName = findViewById(R.id.txtFullName)
-        txtUserType = findViewById(R.id.txtUserType)
-        otherMenu = findViewById(R.id.otherMenu)
-        oldPost = findViewById(R.id.oldPost)
-        archiveOfPosts = findViewById(R.id.archiveOfPosts)
-        ratingReqForHelp = findViewById(R.id.ratingReqForHelp)
-        ratingVolunteerForHelp = findViewById(R.id.ratingVolunteerForHelp)
-        spinnerCategory = findViewById(R.id.spinnerCategory)
-        btnPost = findViewById(R.id.btnPost)
-        recyclerFeed = findViewById(R.id.recycler_feed)
-
-        val postDetail = intent.getSerializableExtra("PostDetail") as? PostDetails
-        //endregion Set variable
-
-        //region Action
-        archiveOfPosts.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, ArchiveOfPostsActivity::class.java)
-            startActivity(intent)
-        })
-
-        oldPost.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, OldPostActivity::class.java)
-            startActivity(intent)
-        })
-
-        otherMenu.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, OtherMenu::class.java)
-            startActivity(intent)
-        })
-
-        btnPost.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, PostActivity::class.java)
-            startActivity(intent)
-        })
-        //endregion Action
-
-        //region On init
-        setRatingUserHeader()
-        setSpinnerCategory()
-        getPosts(postDetail)
-        //endregion On init
     }
 }
